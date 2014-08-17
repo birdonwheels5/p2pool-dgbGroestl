b19e3af3cc555ac1895137e9965ddd4234f83942
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-17 17:07:14 -0400

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -26,7 +26,7 @@ nets = dict(
 	     not (yield bitcoind.rpc_getinfo())['testnet']
         )),
 	SUBSIDY_FUNC = lambda height: __import__('digibyte_subsidy').GetBlockBaseValue(height)
-	POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
+	POW_FUNC = data.hash256
 	BLOCK_PERIOD = 60 # s
 	SYMBOL = 'DGB'
 	CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digibyte') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digibyte/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digibyte'), 'digibyte.conf')
@@ -38,16 +38,16 @@ nets = dict(
 	DUST_THRESHOLD = 0.0001e8
     ),
     digibyteSha_testnet=math.Object(
-        P2P_PREFIX = 'fac3b6da'.decode('hex') #pchmessagestart
-	P2P_PORT = 12024
+        P2P_PREFIX = 'fcc1b7dc'.decode('hex') #pchmessagestart
+	P2P_PORT = 12025
 	ADDRESS_VERSION = 30 #pubkey_address
-	RPC_PORT = 14022
+	RPC_PORT = 14023
 	RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
 	    'digibyteaddress' in (yield bitcoind.rpc_help()) and
 	     not (yield bitcoind.rpc_getinfo())['testnet']
         )),
 	SUBSIDY_FUNC = lambda height: __import__('digibyte_subsidy').GetBlockBaseValue(height)
-	POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
+	POW_FUNC = data.hash256
 	BLOCK_PERIOD = 60 # s
 	SYMBOL = 'DGB'
 	CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digibyte') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digibyte/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digibyte'), 'digibyte.conf')
