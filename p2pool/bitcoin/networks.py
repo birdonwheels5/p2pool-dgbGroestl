9684b038bffa79e7d4ac8f1a2955221fe07c57e4
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-17 18:17:17 -0400

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -22,8 +22,8 @@ nets = dict(
 	ADDRESS_VERSION=30, #pubkey_address
 	RPC_PORT=14022,
 	RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-	    'digibyteaddress' in (yield bitcoind.rpc_help()) and
-	     not (yield bitcoind.rpc_getinfo())['testnet']
+	    'digibyteaddress' in (yield bitcoind.rpc_getinfo())['testnet'] 
+	     and not (yield bitcoind.rpc_help())
         )),
 	SUBSIDY_FUNC=lambda height: __import__('digibyte_subsidy').GetBlockBaseValue(height),
 	POW_FUNC=data.hash256,
