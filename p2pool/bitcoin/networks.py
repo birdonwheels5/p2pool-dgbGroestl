47f561f84f67a595daecbfa3686b955ff072e879
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-17 18:14:02 -0400

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -17,46 +17,46 @@ def check_genesis_block(bitcoind, genesis_block_hash):
 
 nets = dict(
     digibyteSha=math.Object(
-        P2P_PREFIX = 'fac3b6da'.decode('hex') #pchmessagestart
-	P2P_PORT = 12024
-	ADDRESS_VERSION = 30 #pubkey_address
-	RPC_PORT = 14022
-	RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+        P2P_PREFIX='fac3b6da'.decode('hex'), #pchmessagestart
+	P2P_PORT=12024,
+	ADDRESS_VERSION=30, #pubkey_address
+	RPC_PORT=14022,
+	RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
 	    'digibyteaddress' in (yield bitcoind.rpc_help()) and
 	     not (yield bitcoind.rpc_getinfo())['testnet']
         )),
-	SUBSIDY_FUNC = lambda height: __import__('digibyte_subsidy').GetBlockBaseValue(height)
-	POW_FUNC = data.hash256
-	BLOCK_PERIOD = 60 # s
-	SYMBOL = 'DGB'
-	CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digibyte') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digibyte/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digibyte'), 'digibyte.conf')
-	BLOCK_EXPLORER_URL_PREFIX = 'http://altexplorer.net/block/'
-	ADDRESS_EXPLORER_URL_PREFIX = 'http://altexplorer.net/address/'
-	TX_EXPLORER_URL_PREFIX = 'http://altexplorer.net/tx/'
-	SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
-	DUMB_SCRYPT_DIFF = 2**16
-	DUST_THRESHOLD = 0.0001e8
+	SUBSIDY_FUNC=lambda height: __import__('digibyte_subsidy').GetBlockBaseValue(height),
+	POW_FUNC=data.hash256,
+	BLOCK_PERIOD=60, # s
+	SYMBOL='DGB',
+	CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digibyte') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digibyte/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digibyte'), 'digibyte.conf'),
+	BLOCK_EXPLORER_URL_PREFIX='http://altexplorer.net/block/',
+	ADDRESS_EXPLORER_URL_PREFIX='http://altexplorer.net/address/',
+	TX_EXPLORER_URL_PREFIX='http://altexplorer.net/tx/',
+	SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+	DUMB_SCRYPT_DIFF=1,
+	DUST_THRESHOLD=0.0001e8,
     ),
     digibyteSha_testnet=math.Object(
-        P2P_PREFIX = 'fcc1b7dc'.decode('hex') #pchmessagestart
-	P2P_PORT = 12025
-	ADDRESS_VERSION = 30 #pubkey_address
-	RPC_PORT = 14023
-	RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+        P2P_PREFIX='fcc1b7dc'.decode('hex'), #pchmessagestart
+	P2P_PORT=12025,
+	ADDRESS_VERSION=30, #pubkey_address
+	RPC_PORT=14023,
+	RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
 	    'digibyteaddress' in (yield bitcoind.rpc_help()) and
 	     not (yield bitcoind.rpc_getinfo())['testnet']
         )),
-	SUBSIDY_FUNC = lambda height: __import__('digibyte_subsidy').GetBlockBaseValue(height)
-	POW_FUNC = data.hash256
-	BLOCK_PERIOD = 60 # s
-	SYMBOL = 'DGB'
-	CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digibyte') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digibyte/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digibyte'), 'digibyte.conf')
-	BLOCK_EXPLORER_URL_PREFIX = 'http://altexplorer.net/block/'
-	ADDRESS_EXPLORER_URL_PREFIX = 'http://altexplorer.net/address/'
-	TX_EXPLORER_URL_PREFIX = 'http://altexplorer.net/tx/'
-	SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
-	DUMB_SCRYPT_DIFF = 2**16
-	DUST_THRESHOLD = 0.0001e8
+	SUBSIDY_FUNC=lambda height: __import__('digibyte_subsidy').GetBlockBaseValue(height),
+	POW_FUNC=data.hash256,
+	BLOCK_PERIOD=60, # s
+	SYMBOL='DGB',
+	CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digibyte') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digibyte/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digibyte'), 'digibyte.conf'),
+	BLOCK_EXPLORER_URL_PREFIX='http://altexplorer.net/block/',
+	ADDRESS_EXPLORER_URL_PREFIX='http://altexplorer.net/address/',
+	TX_EXPLORER_URL_PREFIX='http://altexplorer.net/tx/',
+	SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+	DUMB_SCRYPT_DIFF=1,
+	DUST_THRESHOLD=0.0001e8,
     ),
 
 )
